291cb91879a640817b4f414cfa72ba4e2e16cb59
James Bensley <jwbensley@gmail.com>
2026-02-04 22:31:31 +0100

    Finishing plotting the discovered peerings

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@
 
 
 import argparse
+from copy import deepcopy
 from enum import Enum
 import json
 import os
@@ -11,6 +12,7 @@ import plotly.offline as po
 
 BASE_DIR = "./results"
 PEERING_DATA = os.path.join(BASE_DIR, "peering_data.json")
+MIRRORED_DATA = os.path.join(BASE_DIR, "peering_data_mirrored.json")
 PEERING_OUTPUT = os.path.join(BASE_DIR, "peerings.html")
 T1_ASNS = {
     174: "Cogent",
@@ -54,12 +56,76 @@ class PeerLocations(Enum):
 cli_args: argparse.Namespace
 
 
-def load_peerings(filename: str) -> dict[str, Any]:
+def load_json(filename: str) -> dict[str, Any]:
     data: dict[str, dict[str, Any]] = json.load(open(filename))
-    return data["peering_data"]
+    return data
 
 
-def plot_peerings(data: dict[str, Any]) -> None:
+def mirror_data(peering_data: dict[str, Any]) -> dict[str, Any]:
+    """
+    Add corresponding entries to peering data, for peerings discoverd on only
+    one side of the peering i.e., if we found 123 is peering for 456, but we
+    don't have a peering under 456 to 123; add the reverse direction because
+    we know it exists from the direction we did find.
+    """
+    mirrored_data = deepcopy(peering_data)
+
+    for local_asn, peers in peering_data["peering_data"].items():
+        for peer_asn, location_peerings in peers["peers"].items():
+
+            local_ptr = mirrored_data["peering_data"]
+
+            if peer_asn not in local_ptr:
+                local_ptr[peer_asn] = {"peers": {}}
+
+            peer_prt = local_ptr[peer_asn]["peers"]
+
+            if local_asn not in peer_prt:
+                peer_prt[local_asn] = {"location_peerings": {}}
+
+            for location, peerings_in_loc in location_peerings[
+                "location_peerings"
+            ].items():
+
+                location_ptr = peer_prt[local_asn]["location_peerings"]
+
+                if location not in location_ptr:
+                    location_ptr[location] = {"peerings_in_loc": {}}
+
+                for peer_type, peerings_by_ver in peerings_in_loc[
+                    "peerings_in_loc"
+                ].items():
+
+                    peerings_ptr = location_ptr[location]["peerings_in_loc"]
+
+                    # Customer and upstream roles need to be reversed
+                    if peer_type == PeerTypes.Customer.name:
+                        peer_type = PeerTypes.Upstream.name
+                    elif peer_type == PeerTypes.Upstream.name:
+                        peer_type = PeerTypes.Customer.name
+
+                    if peer_type not in peerings_ptr:
+                        peerings_ptr[peer_type] = {"peerings_by_ver": {}}
+
+                    version_prt = peerings_ptr[peer_type]["peerings_by_ver"]
+
+                    for addr_family, route in peerings_by_ver[
+                        "peerings_by_ver"
+                    ].items():
+
+                        if addr_family not in version_prt:
+                            version_prt[addr_family] = route
+
+    return mirrored_data["peering_data"]
+
+
+def generate_plot_data(data: dict[str, Any]) -> dict[str, Any]:
+    """
+    Take the data as parsed from JSON, and transpose it into column-wise data
+    for plotting. Also create to data structures for storing the text colour
+    and background colour of each table cell, based on it's value.
+    """
+
     text_colour_map = {
         "no_data": "black",
         "peer": "darkgreen",
@@ -90,25 +156,21 @@ def plot_peerings(data: dict[str, Any]) -> None:
             for _ in PeerLocations
         ]
 
-    col_headings = ["<b>ASN</b>"] + [
-        f"<b>{location.value}</b>" for location in PeerLocations
-    ]
-
     # Values are by column
-    all_data: dict[int, list[list[str]]] = {
+    values: dict[int, list[list[str]]] = {
         t1_asn: [[]] + [[] for _ in PeerLocations] for t1_asn in T1_ASNS.keys()
     }
 
     for t1_asn in T1_ASNS.keys():
 
         for asn_index, local_asn in enumerate(T1_ASNS.keys()):
-            all_data[t1_asn][0].append(
+            values[t1_asn][0].append(
                 f"<b>{local_asn}</b> ({T1_ASNS[local_asn]})"
             )
 
             if t1_asn == local_asn:
-                for i in range(1, len(all_data[t1_asn])):
-                    all_data[t1_asn][i].append("-")
+                for i in range(1, len(values[t1_asn])):
+                    values[t1_asn][i].append("-")
                     fill_colours[t1_asn][i][asn_index] = fill_colour_map[
                         "skip"
                     ]
@@ -119,8 +181,8 @@ def plot_peerings(data: dict[str, Any]) -> None:
                 str(t1_asn) not in data
                 or str(local_asn) not in data[str(t1_asn)]["peers"]
             ):
-                for i in range(1, len(all_data[t1_asn])):
-                    all_data[t1_asn][i].append("")
+                for i in range(1, len(values[t1_asn])):
+                    values[t1_asn][i].append("")
                 continue
 
             locations: dict[str, dict[str, dict[str, Any]]] = data[
@@ -128,7 +190,7 @@ def plot_peerings(data: dict[str, Any]) -> None:
             ]["peers"][str(local_asn)]["location_peerings"]
             for loc_index, location in enumerate(PeerLocations):
                 if not location.name in locations:
-                    all_data[t1_asn][loc_index + 1].append("")
+                    values[t1_asn][loc_index + 1].append("")
                     continue
 
                 peer_types = set(
@@ -138,7 +200,7 @@ def plot_peerings(data: dict[str, Any]) -> None:
                         )
                     )
                 )
-                all_data[t1_asn][loc_index + 1].append(
+                values[t1_asn][loc_index + 1].append(
                     f"{', '.join(peer_types)}"
                 )
 
@@ -156,6 +218,25 @@ def plot_peerings(data: dict[str, Any]) -> None:
                     text_colour_map[mapping]
                 )
 
+    return {
+        "values": values,
+        "fill_colours": fill_colours,
+        "text_colours": text_colours,
+    }
+
+
+def plot_peerings(data: dict[str, Any]) -> None:
+    col_headings = ["<b>ASN</b>"] + [
+        f"<b>{location.value}</b>" for location in PeerLocations
+    ]
+
+    data = generate_plot_data(data)
+    values, fill_colours, text_colours = (
+        data["values"],
+        data["fill_colours"],
+        data["text_colours"],
+    )
+
     fig = go.Figure(
         data=[
             go.Table(  # type: ignore
@@ -167,7 +248,7 @@ def plot_peerings(data: dict[str, Any]) -> None:
                     font=dict(color="black", size=12),
                 ),
                 cells=dict(
-                    values=all_data[list(T1_ASNS.keys())[0]],
+                    values=values[list(T1_ASNS.keys())[0]],
                     line_color="gainsboro",
                     fill_color=fill_colours[list(T1_ASNS.keys())[0]],
                     align="center",
@@ -197,7 +278,7 @@ def plot_peerings(data: dict[str, Any]) -> None:
                             args=[
                                 dict(
                                     cells=dict(
-                                        values=all_data[t1_asn],
+                                        values=values[t1_asn],
                                         line=dict(color="gainsboro"),
                                         fill=dict(color=fill_colours[t1_asn]),
                                         font=dict(
@@ -241,10 +322,21 @@ def plot_peerings(data: dict[str, Any]) -> None:
     )
 
 
+def write_json(data: dict[str, Any], filename: str):
+    json.dump(data, open(filename, "w"), indent=2)
+    print(f"Wrote to {filename}")
+
+
 def parse_cli_args() -> None:
     parser = argparse.ArgumentParser(
         description="Script to plot discovered peering relationships and locations",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--mirrored-output",
+        help="Peering data with mirror values added in",
+        type=str,
+        default=MIRRORED_DATA,
     )
     parser.add_argument(
         "--peering-data",
@@ -265,7 +357,10 @@ def parse_cli_args() -> None:
 
 def main() -> None:
     parse_cli_args()
-    plot_peerings(load_peerings(cli_args.peering_data))
+    peering_data = load_json(cli_args.peering_data)
+    mirrored_data = mirror_data(peering_data)
+    write_json(mirrored_data, cli_args.mirrored_output)
+    plot_peerings(mirrored_data)
 
 
 if __name__ == "__main__":
